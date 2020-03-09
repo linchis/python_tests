be4d55f3708da9addb7561db283d1981e5324aba
Carlos Durán <35487775+linchis@users.noreply.github.com>
2020-03-09 16:41:40 +0100

    Add files via upload
    first upload

---
+++ test1.py
@@ -0,0 +1,47 @@
+from bs4 import BeautifulSoup as bsoup
+from urllib.request import Request, urlopen
+import string
+from collections import OrderedDict
+
+hdr = {'User-Agent': 'Mozilla/5.0'}
+mapa={}
+countries=[]
+cities = []
+for i in string.ascii_uppercase:
+#for i in ['E','F','G']:
+    req = Request(r'https://en.wikipedia.org/wiki/List_of_towns_and_cities_with_100,000_or_more_inhabitants/cityname:_'+i, headers=hdr)
+    page = urlopen(req)
+    soup = bsoup(page, 'html.parser')
+    table_body = soup.find("table",{"class":"wikitable sortable"}).tbody
+    children = table_body.findChildren("tr" , recursive=False)
+    print(len(children))
+    
+    for child in children:
+        brothers = child.findChildren("td" , recursive=False)
+        city = ""
+        country = ""
+        for id,bro in enumerate(brothers):
+            if id==0:
+                #city
+                if hasattr(bro.a,'text'):
+                    city=bro.a.text
+                else:
+                    city="unknow1"
+            else:
+                #country
+                if hasattr(bro.a,'text'):
+                    country=bro.a.text
+                else:
+                    country="unknow2"
+                            
+                if country in mapa:
+                    cities=mapa[country]
+                else:
+                    cities = []
+                cities.append(city)
+                mapa[country]=cities
+        #print(country + ";"+ city)
+#sorted_dict = sorted(mapa,key=lambda t: mapa[0])          
+#print(sorted_dict)
+print(mapa)
+        
